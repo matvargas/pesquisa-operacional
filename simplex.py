578cffe562bc51e1674bfc1a642833a7a0dc4e74
Matheus Vargas <m6.vargas@gmail.com>
2019-09-27 14:48:51 -0300

    Refactoring

--- simplex.py
+++ simplex.py
@@ -1,6 +1,23 @@
 import logging
+from fractions import Fraction
 
 class Simplex:
+
+    def get_tableau_costs_vector(tableau):
+        return tableau[0][int((len(tableau.T) - 1) / 3): len(tableau.T) - 1]
+
+    def get_tableau_b_vector(tableau):
+        return tableau.T[len(tableau.T) - 1][1:]
+
+    def get_tableau_a_matrix(tableau):
+        return tableau[1:, int((len(tableau.T) - 1) / 3): len(tableau.T) - 1]
+
+    def pivotate(tableau, pivotal_line, pivotal_colum, pivot):
+        logging.debug('\n =================================== \n =   PIVOTATING   = \n ===================================')
+
+        # Divide the pivotal line by pivot
+        # tableau = tableau[pivotal_line, :] / pivot
+
 
     def define_viable_bases(tableau):
 
@@ -31,9 +48,28 @@ class Simplex:
 
         logging.debug('\n ======================== \n =   STARTING SIMPLEX   = \n ========================')
 
-        for x in tableau[0][int((len(tableau.T) - 1)/3) : len(tableau.T) - 1]:
-            print(x)
+        c = Simplex.get_tableau_costs_vector(tableau)
+        b = Simplex.get_tableau_b_vector(tableau)
+        a = Simplex.get_tableau_a_matrix(tableau)
 
-    # def pivotate():
+        count = 0
+
+        lowest_value = 99999999
+
+        pivotal_line = -1
+        pivotal_column = -1
+
+        while (count < len(c)):
+            if c[count] < 0:
+                for line in range(0, len(a)):
+                    if b[count] != 0 and a[line][count] != 0:
+                        if Fraction(b[count], a[line][count]) < lowest_value:
+                            lowest_value = Fraction(b[count], a[line][count])
+                            pivotal_line = line
+                            pivotal_column = count
+                logging.debug("Lower value under b/a is: {} on A[{}][{}]".format(lowest_value, pivotal_line, pivotal_column))
+                Simplex.pivotate(tableau, pivotal_line, pivotal_line, lowest_value)
+            count += 1
+
 
 
